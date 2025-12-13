006ac89fd7d1335d3587374668f5aad7b9008784
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2025-12-12 23:04:38 -0500

    DHTML Change Completely redid the text rendering to use 3D2D rendered DHTML panels instead + Font: Coolvetica + Font: Segment + Font: Spicy Sale

--- lua/entities/textscreen.lua
+++ lua/entities/textscreen.lua
@@ -1,8 +1,6 @@
 AddCSLuaFile()
-
 ENT.Type = "anim"
 ENT.Base = "base_anim"
-
 ENT.PrintName = "Text Screen"
 ENT.Author = "Mikey"
 ENT.Spawnable = false
@@ -11,8 +9,11 @@ ENT.AdminSpawnable = false
 ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
 ENT.AutomaticFrameAdvance = true
 
+AddCSLuaFile( "includes/3d2dvgui.lua" )
+
 if CLIENT then
-	surface.CreateFont("NewFont", {
+	include( "includes/3d2dvgui.lua" )
+	surface.CreateFont( "NewFont", {
 		font = "Coolvetica",
 		size = 256,
 		weight = 500,
@@ -25,78 +26,153 @@ if CLIENT then
 		symbol = false,
 		rotary = false,
 		outline = false
-	})
+	} )
 else
-	net.Receive("SetTextscreenText", function() -- This is used to scale the textscreen physics based on the text length
+	resource.AddSingleFile( "resource/fonts/Spicy Sale.ttf" )
+	resource.AddSingleFile( "resource/fonts/Segment.ttf" )
+	resource.AddSingleFile( "resource/fonts/Coolvetica.ttf" )
+	-- This is used to scale the textscreen physics based on the text length
+	net.Receive( "SetTextscreenText", function()
 		local textscreen = net.ReadEntity()
 		local textW, textH = net.ReadFloat(), net.ReadFloat()
-
-		local scale = Vector(0.5, textW, textH)
-
-		textscreen:PhysicsInitBox(Vector(scale.x * -0.5,scale.y * -0.5,scale.z * -0.5),Vector(scale.x * 0.5,scale.y * 0.5,scale.z * 0.5))
-
-		textscreen:SetCollisionBounds(Vector(scale.x * -0.5,scale.y * -0.5,scale.z * -0.5),Vector(scale.x * 0.5,scale.y * 0.5,scale.z * 0.5))
-
-		textscreen:SetMoveType(MOVETYPE_VPHYSICS)
-		textscreen:SetSolid(SOLID_VPHYSICS)
-
-		textscreen:EnableCustomCollisions(true)
-		textscreen:SetCustomCollisionCheck(true)
+		local scale = Vector( 0.5, textW, textH )
+		textscreen:PhysicsInitBox( Vector( scale.x * -0.5, scale.y * -0.5, scale.z * -0.5 ), Vector( scale.x * 0.5, scale.y * 0.5, scale.z * 0.5 ) )
+		textscreen:SetCollisionBounds( Vector( scale.x * -0.5, scale.y * -0.5, scale.z * -0.5 ), Vector( scale.x * 0.5, scale.y * 0.5, scale.z * 0.5 ) )
+		textscreen:SetMoveType( MOVETYPE_VPHYSICS )
+		textscreen:SetSolid( SOLID_VPHYSICS )
+		textscreen:EnableCustomCollisions( true )
+		textscreen:SetCustomCollisionCheck( true )
 		textscreen:CollisionRulesChanged()
-
-		textscreen:SetCollisionGroup(COLLISION_GROUP_WORLD)
-
+		textscreen:SetCollisionGroup( COLLISION_GROUP_WORLD )
 		local mass = 50
-
-		textscreen:GetPhysicsObject():SetMass(mass)
-
-
+		textscreen:GetPhysicsObject():SetMass( mass )
 		textscreen:SetSolidFlags( 0 )
 		textscreen:AddSolidFlags( FSOLID_CUSTOMRAYTEST )
 		textscreen:AddSolidFlags( FSOLID_CUSTOMBOXTEST )
-	end)
+	end )
 end
 
 function ENT:Initialize()
 	if SERVER then
-		self:SetModel("models/hunter/blocks/cube05x05x05.mdl")
-		self:SetMaterial("models/wireframe")
-		self:SetRenderMode(RENDERMODE_TRANSCOLOR)
+		self:SetModel( "models/hunter/blocks/cube05x05x05.mdl" )
+		self:SetMaterial( "models/wireframe" )
+		self:SetRenderMode( RENDERMODE_TRANSCOLOR )
 	end
 
 	if CLIENT then
-		self.parsedMarkup = markup.Parse("")
-		self:DrawShadow(false)
+		self.pixelScale = 0.2
+		self.size = Vector( 0, 0, 0 )
+		self.htmlPanel = vgui.Create( "DHTML" )
+		self.htmlPanel:SetSize( self.size[1], self.size[2] )
+		self:DrawShadow( false )
 	end
 end
 
 if CLIENT then
+	function ENT:SetSize( size )
+		self.size = size
+		self.htmlPanel:SetSize( self.size[1], self.size[2] )
+	end
 
-	function ENT:SetText(text)
-		self.parsedMarkup = markup.Parse(text)
-		local textW, textH = self.parsedMarkup:GetWidth(), self.parsedMarkup:GetHeight()
-		local scale = Vector(0.5, textW, textH)
+	function ENT:SetText( text )
+		local modelMaxs = self:OBBMaxs()
 
-		local modelMin, modelMax = self:GetModelBounds()
+		local newHtml = [[
+			<head>
+				<meta name="referrer" content="origin" />
+				<style>
+					@font-face {
+						font-family: 'Spicy Sale'; /* The name you will use in CSS */
+						src: url('asset://garrysmod/resource/fonts/Spicy Sale.ttf') format('truetype'); /* Path relative to the GMod root, using the file:// protocol */
+					}
+					@font-face {
+						font-family: 'Segment'; /* The name you will use in CSS */
+						src: url('asset://garrysmod/resource/fonts/Segment.ttf') format('truetype'); /* Path relative to the GMod root, using the file:// protocol */
+					}
+					@font-face {
+						font-family: 'Coolvetica'; /* The name you will use in CSS */
+						src: url('asset://garrysmod/resource/fonts/Coolvetica.ttf') format('truetype'); /* Path relative to the GMod root, using the file:// protocol */
+					}
+					body {
+						background: transparent;
+						overflow: hidden;
+						overflow-wrap: anywhere;
+						margin: 0;
+						padding: 0;
+						transform: translateZ(0);
+						--font: 'Arial';
+						--size: 15;
+						--font-style: none;
+						--color: rgb(255, 255, 255);
+						--shadow-color: rgba( 0, 0, 0, 0.0 );
+						--shadow-blur: 1;
+						--shadow-x: 0;
+						--shadow-y: 0;
+						--stroke: 1;
+						--stroke-color: rgb( 0, 0, 0 );
+					}
 
-		local modelSize = math.min(modelMax.x - modelMin.x, modelMax.y - modelMin.y, modelMax.z - modelMin.z) - 0.5
+					.container {
+						display: flex;
+						width: fit-content;
+						height: fit-content;
+						padding: 2em;
+						align-items: center;
+						justify-content: center;
+						background: #fff0;
+						max-width: 2000px;
+						max-height: 2000px;
+					}
+					text {
+						display: inline;
+						position: relative;
+						text-align: center;
+						
+						white-space: pre-wrap;
+						color: var(--color);
+						-webkit-text-stroke: calc( var( --stroke ) * 1px + 1px ) var( --stroke-color );
+						font-family: var(--font);
+						font-size: calc( var(--size) * 1em );
+						font-style: var(--font-style);
+						text-shadow: calc( var( --shadow-x ) * 1em ) calc( var( --shadow-y ) * 1em ) calc( var( --shadow-blur ) * 0.1em ) var( --shadow-color );
+					}
+				</style>
+			</head>
+			<body>
+				<div class="container" id="main">
+		]]
 
-		local textSize = textH
+		text = string.Replace( text, ">\n", ">" ) -- Remove line breaks right after tags
+		text = string.TrimRight( text )
 
-		local sclMat = Matrix()
-		sclMat:SetScale(scale / modelSize * 32 / textSize)
+		newHtml = newHtml .. text
+		newHtml = newHtml .. "</div>"
+		newHtml = newHtml .. "</body>"
 
-		self:EnableMatrix("RenderMultiply", sclMat)
+		self.htmlPanel:SetHTML( newHtml )
 
-		self:SetRenderBounds(-scale * 32 / textSize * 0.5, scale * 32 / textSize * 0.5)
+		--local scale = Vector( 0.5, self.size[1] * self.pixelScale, self.size[2] * self.pixelScale )
 
-		if LocalPlayer() ~= self:GetNW2Entity("owner") then return end
+		self.htmlPanel:AddFunction( "textscreen", "resizeTextscreen", function( w, h )
+			local scale = Vector( 0.5, w * self.pixelScale, h * self.pixelScale )
+			local sclMat = Matrix()
+			sclMat:SetScale( scale / ( modelMaxs[1] - 0.25 ) / 2 )
+			self:EnableMatrix( "RenderMultiply", sclMat )
+			self:SetRenderBounds( -scale, scale )
+			self.size = Vector( w, h, 0 )
+			if LocalPlayer() ~= self:GetNW2Entity( "owner" ) then return end
+			net.Start( "SetTextscreenText" )
+			net.WriteEntity( self )
+			net.WriteFloat( scale[2] )
+			net.WriteFloat( scale[3] )
+			net.SendToServer()
+		end )
 
-		net.Start("SetTextscreenText")
-			net.WriteEntity(self)
-			net.WriteFloat(textW * 32 / textSize)
-			net.WriteFloat(textH * 32 / textSize)
-		net.SendToServer()
+		self.htmlPanel:QueueJavascript( [[
+			const elem = document.getElementById( "main" );
+			var rect = elem.getBoundingClientRect();
+			textscreen.resizeTextscreen( rect.width, rect.height );
+		]] );
 	end
 
 	function ENT:Think()
@@ -110,28 +186,34 @@ if CLIENT then
 		end
 	end
 
-	function ENT:DrawTranslucent()
-		local textW, textH = self.parsedMarkup:GetWidth(), self.parsedMarkup:GetHeight()
-		local textSize = textH
+	function ENT:OnRemove()
+		self.htmlPanel:Remove()
+	end
+
+	function ENT:Draw()
 		--self:DrawModel()
 		local mat = Matrix()
-		mat:Translate(self:WorldSpaceCenter())
-		mat:Rotate(self:GetAngles())
-		mat:Rotate(Angle(0,90,90))
+		mat:Translate( self:WorldSpaceCenter() )
+		mat:Rotate( self:GetAngles() )
+		mat:Rotate( Angle( 0, 90, 90 ) )
+		mat:Translate( Vector( -self.size[1], self.size[2], 0 ) * 0.5 * self.pixelScale )
 
+		vgui.Start3D2D( mat:GetTranslation(), mat:GetAngles(), self.pixelScale )
+			self.htmlPanel:Paint3D2D()
+		vgui.End3D2D()
 
-		cam.Start3D2D( mat:GetTranslation(), mat:GetAngles(), 32 / textSize)
-			self.parsedMarkup:Draw(0, 0, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-		cam.End3D2D()
+		mat = Matrix()
+		mat:Translate( self:WorldSpaceCenter() )
+		mat:Rotate( self:GetAngles() )
+		mat:Rotate( Angle( 0, -90, 90 ) )
+		mat:Translate( Vector( -self.size[1], self.size[2], 0 ) * 0.5 * self.pixelScale )
 
-		mat:Rotate(Angle(180,0,0))
+		vgui.Start3D2D( mat:GetTranslation(), mat:GetAngles(), self.pixelScale )
+			self.htmlPanel:Paint3D2D()
+		vgui.End3D2D()
 
-		cam.Start3D2D( mat:GetTranslation(), mat:GetAngles(), 32 / textSize)
-			self.parsedMarkup:Draw(0, 0, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-		cam.End3D2D()
-
-		local renderCube = IsValid(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass() == "gmod_tool" and LocalPlayer():GetTool().Mode == "textscreen_revamped"
-		renderCube = renderCube or (IsValid(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass() == "weapon_physgun")
+		local renderCube = IsValid( LocalPlayer():GetActiveWeapon() ) and LocalPlayer():GetActiveWeapon():GetClass() == "gmod_tool"
+		renderCube = renderCube or IsValid( LocalPlayer():GetActiveWeapon() ) and LocalPlayer():GetActiveWeapon():GetClass() == "weapon_physgun"
 
 		if renderCube then
 			self:DrawModel()
--- lua/weapons/gmod_tool/stools/textscreen_revamped.lua
+++ lua/weapons/gmod_tool/stools/textscreen_revamped.lua
@@ -1,33 +1,42 @@
 AddCSLuaFile()
 
 if SERVER then
-	util.AddNetworkString("SetTextscreenText")
+	util.AddNetworkString( "SetTextscreenText" )
 end
 
 TOOL.Author = "Mikey"
 TOOL.Name = "#tool.textscreen_revamped.name"
 TOOL.Category = "Text Screens Revamped"
+TOOL.ClientConVar["size_x"] = 500
+TOOL.ClientConVar["size_y"] = 300
+
 
 if CLIENT then
 	TOOL.Information = {
-		{name = "left"},
+		{
+			name = "left"
+		},
 	}
-	language.Add("tool.textscreen_revamped.name", "Text Screen Revamped")
-	language.Add("tool.textscreen_revamped.desc", "Creates a text screen.")
-	language.Add("tool.textscreen_revamped.left", "Create a Text Screen.")
 
-	net.Receive("SetTextscreenText", function()
+	language.Add( "tool.textscreen_revamped.name", "Text Screen Revamped" )
+	language.Add( "tool.textscreen_revamped.desc", "Creates a text screen." )
+	language.Add( "tool.textscreen_revamped.left", "Create a Text Screen." )
+
+	net.Receive( "SetTextscreenText", function()
 		local ply = net.ReadEntity()
 		local ent = net.ReadEntity()
 		local updated = net.ReadBool()
+		local sizeX = net.ReadFloat()
+		local sizeY = net.ReadFloat()
 
 		-- Since traces are going through the entity, I'm playing a fake tool sound just so the player knows they're doing something.
 		if LocalPlayer() == ply and updated then
-			surface.PlaySound("Airboat.FireGunRevDown")
+			surface.PlaySound( "Airboat.FireGunRevDown" )
 		end
 
-		ent:SetText(ply.textscreen_revamped.currentTextScreenText)
-	end)
+		ent:SetSize( Vector( sizeX, sizeY, 0 ) )
+		ent:SetText( ply.textscreen_revamped.currentTextScreenText )
+	end )
 end
 
 function TOOL:LeftClick( trace )
@@ -38,15 +47,20 @@ function TOOL:LeftClick( trace )
 	ent:SetAngles( trace.HitNormal:Angle() )
 	ent:Spawn()
 	ent:Activate()
-	ent:SetNW2Entity("owner", self:GetOwner())
+	ent:SetNW2Entity( "owner", self:GetOwner() )
 
-	net.Start("SetTextscreenText")
-	net.WriteEntity(self:GetOwner())
-	net.WriteEntity(ent)
-	net.WriteBool(false)
-	net.Broadcast()
+	timer.Simple( 0.1, function()
+		if not IsValid( ent ) then return end
+		net.Start( "SetTextscreenText" )
+		net.WriteEntity( self:GetOwner() )
+		net.WriteEntity( ent )
+		net.WriteBool( false )
+		net.WriteFloat( self:GetClientNumber( "size_x" ) )
+		net.WriteFloat( self:GetClientNumber( "size_y" ) )
+		net.Broadcast()
+	end )
 
-	if IsValid(trace.Entity) then
+	if IsValid( trace.Entity ) then
 		ent:SetParent( trace.Entity )
 	end
 
@@ -60,15 +74,15 @@ end
 
 function TOOL:RightClick( trace )
 	if CLIENT then return false end
-
 	local ent = trace.Entity
 
-	if IsValid(ent) and ent:GetClass() == "textscreen" then
-		net.Start("SetTextscreenText")
-		net.WriteEntity(self:GetOwner())
-		net.WriteEntity(ent)
-		net.WriteBool(true)
+	if IsValid( ent ) and ent:GetClass() == "textscreen" then
+		net.Start( "SetTextscreenText" )
+		net.WriteEntity( self:GetOwner() )
+		net.WriteEntity( ent )
+		net.WriteBool( true )
 		net.Broadcast()
+
 		return true
 	end
 
@@ -76,99 +90,109 @@ function TOOL:RightClick( trace )
 end
 
 -- Create a json file that keeps the settings between sessions
-
 if CLIENT then
 	local defaultFileName = "textscreen_revamped/default.txt"
 	local lastPresetFiles = ""
 
-	if not file.Exists("textscreen_revamped", "DATA") then
-		file.CreateDir("textscreen_revamped")
-		file.Write(defaultFileName, [[<font=NewFont><colour=255,255,255,255>garry's mod]])
+	if not file.Exists( "textscreen_revamped", "DATA" ) then
+		file.CreateDir( "textscreen_revamped" )
 	end
 
-	function TOOL.BuildCPanel(panel)
-		local presetPanel = vgui.Create("ControlPresets")
+	if not file.Exists( defaultFileName, "DATA" ) then
+		file.Write( defaultFileName, [[
+<text style="
+--font: Coolvetica;
+--size: 5;
+--color: rgb( 180, 220, 255 );
+--shadow-color: rgb( 0, 180, 255 );
+">
+garry's mod
+		]] )
+	end
 
+	function TOOL.BuildCPanel( panel )
+		local presetPanel = vgui.Create( "ControlPresets" )
 		local files, _ = file.Find( "textscreen_revamped/*.txt", "DATA" )
 
-		for _, fileName in pairs(files) do
-			fileName = string.sub(fileName, 1, -5) -- Remove .txt from filename
-			presetPanel:AddOption(fileName, nil)
+		for _, fileName in pairs( files ) do
+			fileName = string.sub( fileName, 1, -5 ) -- Remove .txt from filename
+			presetPanel:AddOption( fileName, nil )
 		end
 
-		local textEdit = vgui.Create("DTextEntry")
-		textEdit:Dock(TOP)
-		textEdit:SetTall(200)
-		textEdit:SetMultiline(true)
+		local textEdit = vgui.Create( "DTextEntry" )
+		textEdit:Dock( TOP )
+		textEdit:SetTall( 200 )
+		textEdit:SetMultiline( true )
+		local lastSavedTxt = file.Read( defaultFileName, "DATA" ) or [[
+		<text style="
+		--font: Coolvetica;
+		--size: 5;
+		--color: rgb( 180, 220, 255 );
+		--shadow-color: rgb( 0, 180, 255 );
+		">
+		garry's mod
+		]]
+		textEdit:SetValue( lastSavedTxt )
+		textEdit:SetUpdateOnType( true )
 
-		local lastSavedTxt = file.Read( defaultFileName, "DATA" ) or [[<font=NewFont><colour=255,255,255,255>garry's mod]]
-
-		textEdit:SetValue(lastSavedTxt)
-		textEdit:SetUpdateOnType(true)
-
-
-		function textEdit:OnValueChange(text)
+		function textEdit:OnValueChange( text )
 			LocalPlayer().textscreen_revamped.currentTextScreenText = text
 		end
 
-		function presetPanel:OnSelect(id, name, data)
-			local contents = file.Read("textscreen_revamped/" .. name .. ".txt", "DATA")
-			textEdit:SetValue(contents)
+		function presetPanel:OnSelect( id, name, data )
+			local contents = file.Read( "textscreen_revamped/" .. name .. ".txt", "DATA" )
+			textEdit:SetValue( contents )
 			LocalPlayer().textscreen_revamped.currentTextScreenText = contents
 		end
 
-		function presetPanel:QuickSaveInternal(name) -- Override this bitch since we're not using cvars
-			name = string.lower(name)
-
+		-- Override this bitch since we're not using cvars
+		function presetPanel:QuickSaveInternal( name )
+			name = string.lower( name )
 			lastPresetFiles = ""
-
-			file.Write("textscreen_revamped/" .. name .. ".txt", textEdit:GetValue())
-
+			file.Write( "textscreen_revamped/" .. name .. ".txt", textEdit:GetValue() )
 			presetPanel:Clear()
-
 			files, directories = file.Find( "textscreen_revamped/*.txt", "DATA" )
 
-			for _, fileName in pairs(files) do
-				fileName = string.sub(fileName, 1, -5) -- Remove .txt from filename
-				presetPanel:AddOption(fileName, nil)
+			for _, fileName in pairs( files ) do
+				fileName = string.sub( fileName, 1, -5 ) -- Remove .txt from filename
+				presetPanel:AddOption( fileName, nil )
 				lastPresetFiles = lastPresetFiles .. fileName
 			end
 		end
 
-		timer.Remove("Textscreen_Revamped_WatchFiles")
-		timer.Create("Textscreen_Revamped_WatchFiles", 0.2, 0, function()
+		timer.Remove( "Textscreen_Revamped_WatchFiles" )
+
+		timer.Create( "Textscreen_Revamped_WatchFiles", 0.2, 0, function()
 			if not presetPanel then return end
 			files, directories = file.Find( "textscreen_revamped/*.txt", "DATA" )
-
 			local newPresetFiles = ""
 
-			for _, fileName in pairs(files) do
+			for _, fileName in pairs( files ) do
 				newPresetFiles = newPresetFiles .. fileName
 			end
 
 			if newPresetFiles ~= lastPresetFiles then
 				lastPresetFiles = ""
-
 				presetPanel:Clear()
 
-				for _, fileName in pairs(files) do
-					fileName = string.sub(fileName, 1, -5) -- Remove .txt from filename
-					presetPanel:AddOption(fileName, nil)
+				for _, fileName in pairs( files ) do
+					fileName = string.sub( fileName, 1, -5 ) -- Remove .txt from filename
+					presetPanel:AddOption( fileName, nil )
 					lastPresetFiles = lastPresetFiles .. fileName
 				end
 			end
 
 			lastPresetFiles = newPresetFiles
-		end)
+		end )
 
-		panel:AddItem(presetPanel)
-		panel:AddItem(textEdit)
+		panel:AddItem( presetPanel )
+		panel:AddItem( textEdit )
 	end
 
-	hook.Add("InitPostEntity", "TextscreenRevamped_PlayerInit", function()
+	hook.Add( "InitPostEntity", "TextscreenRevamped_PlayerInit", function()
 		local ply = LocalPlayer()
 		ply.textscreen_revamped = {}
 		local lastSavedTxt = file.Read( defaultFileName, "DATA" ) or [[<font=NewFont><colour=255,255,255,255>garry's mod]]
 		ply.textscreen_revamped.currentTextScreenText = lastSavedTxt
-	end)
+	end )
 end
