3bd62ac1494ee8635618f4430bc5fae9326f9a79
Mikey-Mikey <mikeygmoore07@gmail.com>
2026-01-30 00:02:42 -0500

    Make the textscreen bounds not show by default

--- lua/autorun/client/textscreen_revamped_cvars.lua
+++ lua/autorun/client/textscreen_revamped_cvars.lua
@@ -1,4 +1,4 @@
 TEXTSCREEN_REVAMPED = TEXTSCREEN_REVAMPED or {}
 
 TEXTSCREEN_REVAMPED.RenderDistanceCVar = CreateClientConVar( "textscreen_render_distance", 4000, true, false )
-TEXTSCREEN_REVAMPED.ShowTextscreenBoundsCVar = CreateClientConVar( "textscreen_show_bounds", 1, true, false )
+TEXTSCREEN_REVAMPED.ShowTextscreenBoundsCVar = CreateClientConVar( "textscreen_show_bounds", 0, true, false )
